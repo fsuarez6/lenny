da5d8b91a64b15d08660bfbef10fbee0237a0f69
fsuarez6 <fsuarez6@gmail.com>
2018-04-05 18:37:27 +0800

    Complete BimanualPlanner.find_ik_solutions

--- lenny_openrave/scripts/bimanual_pick_and_place.py
+++ lenny_openrave/scripts/bimanual_pick_and_place.py
@@ -6,6 +6,8 @@ import criutils as cu
 import raveutils as ru
 import robotsp as rtsp
 import openravepy as orpy
+# Bimanual planner
+from lenny_openrave.bimanual import BimanualPlanner
 
 
 np.set_printoptions(precision=6, suppress=True)
@@ -16,36 +18,24 @@ if not env.Load(world_xml):
   raise Exception('Failed to load world: {}'.format(world_xml))
 robot = env.GetRobot('robot')
 # orpy.RaveSetDebugLevel(orpy.DebugLevel.Fatal)
-env.SetViewer('qtcoin')
-left_manip = robot.GetManipulator('arm_left_tool0')
-right_manip = robot.GetManipulator('arm_right_tool0')
-if left_manip is None:
-  raise Exception('Failed to find manip: arm_left_tool0')
-if right_manip is None:
-  raise Exception('Failed to find manip: arm_right_tool0')
-left_indices = left_manip.GetArmIndices()
-right_indices = right_manip.GetArmIndices()
-# Load IKFast for each manip
-iktype = orpy.IkParameterizationType.Transform6D
-robot.SetActiveManipulator(left_manip)
-if not ru.kinematics.load_ikfast(robot, iktype):
-  raise Exception('Failed to load left IKFast {0}'.format(iktype.name))
-robot.SetActiveManipulator(right_manip)
-if not ru.kinematics.load_ikfast(robot, iktype):
-  raise Exception('Failed to load right IKFast {0}'.format(iktype.name))
-# Find the IK solution for both arms
-robot.SetActiveManipulator(left_manip)
+# env.SetViewer('qtcoin')
+bimanual = BimanualPlanner(robot)
+bimanual.set_left_manipulator('arm_left_tool0')
+bimanual.set_right_manipulator('arm_right_tool0')
+bimanual.set_torso_joint('torso_joint_b1')
+# Load IKFast
+if not bimanual.load_ikfast():
+  raise Exception('Failed to load IKFast')
+# Find the IK solutions for both arms
 Tleft = env.GetKinBody('cube03').GetTransform()
 Tleft[:3,3] += [0, 0, 0.065]
-left_solutions = ru.kinematics.find_ik_solutions(robot, Tleft, iktype,
-                                                            collision_free=True)
-robot.SetActiveManipulator(right_manip)
 Tright = env.GetKinBody('cube01').GetTransform()
 Tright[:3,3] += [0, 0, 0.065]
-right_solutions = ru.kinematics.find_ik_solutions(robot, Tright, iktype,
-                                                            collision_free=True)
-if len(left_solutions) == 0 or len(right_solutions) == 0:
-  raise Exception('Failed to find IK solutions for both arms')
+t0 = time.time()
+solutions = bimanual.find_ik_solutions(Tleft, Tright)
+duration_ik = time.time() - t0
+print (len(solutions), duration_ik)
+
 # Reduce the velocity limits
 velocity_limits = robot.GetDOFVelocityLimits()
 robot.SetDOFVelocityLimits(0.1*velocity_limits)
--- lenny_openrave/src/lenny_openrave/bimanual.py
+++ lenny_openrave/src/lenny_openrave/bimanual.py
@@ -1,55 +1,116 @@
 #! /usr/bin/env python
 import math
+import itertools
 import numpy as np
 import raveutils as ru
 import robotsp as rtsp
 import openravepy as orpy
 
 
-class Bimanual(object):
+class BimanualPlanner(object):
   def __init__(self, robot):
     self.robot = robot
     self.env = self.robot.GetEnv()
     self.joint_names = [j.GetName() for j in self.robot.GetJoints()]
     # Initialize variables
-    self.iktype = None
     self.left_manip = None
     self.right_manip = None
+    self.iktype = orpy.IkParameterizationType.Transform6D
     self.torso_joint = None
 
-  def load_ikfast(self):
+  def _estimate_arm_lenght(self, manip):
+    joints = [j for j in self.robot.GetDependencyOrderedJoints()
+                            if j.GetJointIndex() in manip.GetArmIndices()]
+    anchor = joints[0].GetAnchor()
+    eetrans = manip.GetEndEffectorTransform()[0:3,3]
+    armlength = 0
+    for j in reversed(joints):
+        armlength += math.sqrt(sum((eetrans-j.GetAnchor())**2))
+        eetrans = j.GetAnchor()
+    return anchor,armlength
+
+  def _get_anchor_joint(self, manip):
+    for j in self.robot.GetDependencyOrderedJoints():
+      if j.GetJointIndex() in manip.GetArmIndices():
+       anchor_joint = j
+       break
+    return anchor_joint
+
+  def lazy_reachability_check(self, manip, point):
+    anchor,armlenght = self._estimate_arm_lenght(manip)
+    distance = np.linalg.norm(point - anchor)
+    reachable = distance < armlenght
+    return reachable
+
+  def load_ikfast(self, freeinc=np.pi/6.):
     success = False
-    if self.left_manip is not None:
-      self.robot.SetActiveManipulator(left_manip)
-      success = ru.kinematics.load_ikfast(self.robot, iktype)
-    if self.right_manip is not None and success:
-      self.robot.SetActiveManipulator(self.right_manip)
-      success = ru.kinematics.load_ikfast(self.robot, iktype)
-    if success:
-      self.iktype = iktype
+    from openravepy.databases.inversekinematics import InverseKinematicsModel
+    manipulators = [self.left_manip, self.right_manip]
+    if None not in manipulators:
+      for manip in manipulators:
+        ikmodel = InverseKinematicsModel(self.robot, iktype=self.iktype,
+                                                                    manip=manip)
+        success = ikmodel.load(freeinc=[freeinc])
+        if not success:
+          break
     return success
 
-  def find_ik_solutions(self, Tleft, Tright, torso_step=np.pi/6.,
-                                                          collision_free=True):
-    if self.torso_joint is not None and torso_step > 0:
-      lower = float(self.torso_joint.GetLimits()[0])
-      upper = float(self.torso_joint.GetLimits()[1])
-      num_steps = math.ceil((upper - lower) / float(torso_step))
-      torso_angles = np.linspace(lower, upper, num_steps)
-    left_solutions = []
-    right_solutions = []
-    for angle in torso_angles:
-      solutions = []
-      self.robot.SetActiveManipulator(self.left_manip)
-      left_sols = ru.kinematics.find_ik_solutions(self.robot, Tleft, self.iktype, collision_free=True)
+  def find_ik_solutions(self, Tleft, Tright, collision_free=True,
+                                                              lazy_check=False):
+    ## Lazy check for reachability
+    if lazy_check:
+      reach_left = self.lazy_reachability_check(self.left_manip, Tleft[:3,3])
+      reach_right = self.lazy_reachability_check(self.right_manip, Tright[:3,3])
+      if not (reach_left and reach_right):
+        return []
+    solutions = []
+    # IKFast for the left arm
+    self.robot.SetActiveManipulator(self.left_manip)
+    left_sols = ru.kinematics.find_ik_solutions(self.robot, Tleft,
+                                    self.iktype, collision_free=collision_free)
+    if len(left_sols) > 0:
+      # IKFast for the right arm
+      self.robot.SetActiveManipulator(self.right_manip)
+      right_sols = ru.kinematics.find_ik_solutions(self.robot, Tright,
+                                    self.iktype, collision_free=collision_free)
+      if len(right_sols) > 0:
+        # The solutions list is the combinations of left and right sols
+        indices = np.hstack((self.left_indices,self.right_indices))
+        for qleft,qright in itertools.product(left_sols, right_sols):
+          config = np.hstack((qleft,qright))
+          self.robot.SetDOFValues(config, indices)
+          valid_config = True
+          if collision_free:
+            if self.robot.CheckSelfCollision():
+              valid_config = False
+          if valid_config:
+            solutions.append(config)
+    return solutions
+
+  def sample_torso_angles(self, torso_step, lower_limit=None, upper_limit=None):
+    if lower_limit is None:
+      lower_limit = float(self.torso_joint.GetLimits()[0])
+    if upper_limit is None:
+      upper_limit = float(self.torso_joint.GetLimits()[1])
+    lower_angles = np.arange(0, lower_limit, torso_step*np.sign(lower_limit))
+    upper_angles = np.arange(0, upper_limit, torso_step*np.sign(upper_limit))
+    return np.unique(np.hstack((lower_angles,upper_angles)))
 
   def set_left_manipulator(self, manip_name):
+    success = False
     self.left_manip = self.robot.GetManipulator(manip_name)
-    self.left_indices = self.left_manip.GetArmIndices()
+    if self.left_manip is not None:
+      success = True
+      self.left_indices = self.left_manip.GetArmIndices()
+    return success
 
   def set_right_manipulator(self, manip_name):
+    success = False
     self.right_manip = self.robot.GetManipulator(manip_name)
-    self.right_indices = self.right_manip.GetArmIndices()
+    if self.right_manip is not None:
+      success = True
+      self.right_indices = self.right_manip.GetArmIndices()
+    return success
 
   def set_torso_joint(self, joint_name):
     success = False
